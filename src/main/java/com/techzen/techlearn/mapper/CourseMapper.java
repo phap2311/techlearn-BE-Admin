6811c5c451d2c69b9c3515c9ad7e2e24960ea426
Pham Thanh Truc <kientruc582@gmail.com>
2024-10-03 08:58:30 +0700

    update code add teacher in course

--- src/main/java/com/techzen/techlearn/mapper/CourseMapper.java
+++ src/main/java/com/techzen/techlearn/mapper/CourseMapper.java
@@ -17,6 +17,7 @@ import java.util.stream.Collectors;
 @Mapper(componentModel = "spring")
 public interface CourseMapper {
     @Mapping(target = "techStack", source = "techStackEntities", qualifiedByName = "mapTechStackEntitiesToDTOs")
+    @Mapping(target = "teacher", source = "teachers", qualifiedByName = "mapTeacherEntitiesToResponseDTOs")
     CourseResponseDTO toCourseResponseDTO(CourseEntity courseEntity);
 
     CourseEntity toCourseEntity(CourseRequestDTO courseRequestDTO);
@@ -27,6 +28,18 @@ public interface CourseMapper {
                 .map(entity -> TechStackResponseDTO.builder()
                         .id(entity.getId())
                         .name(entity.getName())
+                        .build())
+                .collect(Collectors.toList());
+    }
+
+    @Named("mapTeacherEntitiesToResponseDTOs")
+    default List<TeacherResponseDTO> mapTeacherEntitiesToResponseDTOs(List<TeacherEntity> teachers) {
+        return teachers.stream()
+                .map(teacher -> TeacherResponseDTO.builder()
+                        .id(teacher.getId())
+                        .name(teacher.getName())
+                        .avatar(teacher.getAvatar())
+                        .color(teacher.getColor()   )
                         .build())
                 .collect(Collectors.toList());
     }
